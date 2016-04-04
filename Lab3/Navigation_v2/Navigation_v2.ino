235f6e85ac269a0092f1ed7464902147c41dad18
sanchezg7 <gerardo.sanchez94@gmail.com>
2016-04-04 15:50:36 -0400

    New WallFollowing for Navigation. Need to address the corner and left turn methodologies

--- Lab3/Navigation_v2/Navigation_v2.ino
+++ Lab3/Navigation_v2/Navigation_v2.ino
@@ -25,6 +25,7 @@ int go = 0; //go flag for Navigation
 //SENSOR Readings
 int shrtF = 0;
 int shrtL = 0;
+int shrtR = 0;
 
 //SERVOR Velocities
 int rVel;
@@ -64,7 +65,7 @@ void loop() {
     }
     
   }
-  printLCD(trueDist(SFSensor, 10), trueDist(SLSensor, 10), 90, 90);
+  printLCD(trueDist(SFSensor, 20), trueDist(SLSensor, 20), trueDist_L(LFSensor, 20), 90, 90);
 }
 
 
@@ -82,24 +83,44 @@ int trueDist(int sensor, int loops){
   return value / loops;
 }
 
-void printLCD(int shrtF, int shrtL, int lVel, int rVel)
+int trueDist_L(int sensor, int loops){
+  int value = 0;
+  for(int i = 0; i<loops; ++i)
+  {
+    int distance = computeLongDistance(analogRead(sensor));
+    if(distance != -1)
+    {
+      value+= distance;
+    }
+  }
+  return value / loops;
+  
+}
+
+void printLCD(int shrtF, int shrtL, int lngF, int lVel, int rVel)
 {
   
 
   if( shrtF != -1)
   {
      lcd.setCursor(0,1);
-     lcd.print("F: ");
+     lcd.print("F:");
      lcd.print(shrtF);
      lcd.print(" ");
   }
   if(shrtL != -1)
   {
-    lcd.setCursor(6,1);
-    lcd.print("L: ");
+    lcd.setCursor(5,1);
+    lcd.print("L:");
     lcd.print(shrtL);
     lcd.print(" ");
   }
+
+//  if(lngF != -1){
+//    lcd.setCursor(10, 1);
+//    lcd.print("l:");
+//    lcd.print(lngF);
+//  }
 
   if(lVel != 90)
   {
@@ -118,108 +139,104 @@ void printLCD(int shrtF, int shrtL, int lVel, int rVel)
 }
 
 void Navigation(){
+  lcd.clear();
   int lngF; //long Front value
   bool cornerFlg = false;
   bool leftTrnFlg = false;
-  
-  while(1){
+
+  int counter = 0;
+  int wfCounter = 0;
+  while(1)
+  {
     shrtF = trueDist(SFSensor, 10);
     shrtL = trueDist(SLSensor, 10);
-    lngF = trueDist(LFSensor, 10);
+    shrtR = trueDist(SRSensor, 10);
+    
+    printLCD(shrtF, shrtL, lngF, lVel, rVel);
 
     //CORNER case - YELLOW
     //LEFT turn - GREEN
     //too FAR - TEAL
     //too CLOSE - RED
-
     
 
-      //GETTING CLOSE TO WALL
-      if(shrtF < 5)
+      //WALL FOLLOWING
+      if(shrtF > 6)
       {
-          //STOP the robot and resample
-          if(!cornerFlg)
+        //we dont care about the FRONT wall
+          if(shrtL <= 5 ) //TOO CLOSE TO THE WALL
           {
-            myMover.follow_vel(90,90);
-            lcd.clear();
-            lcd.setBacklight(YELLOW);
-            lcd.print("Corner case check");
-            delay(1000);
-            cornerFlg = true;  
-            continue;
-          }else if(shrtL <= 5) 
-          { //check for for six if test doesnt work
-            //CORNER confirmed (right turn, to proceed)
-            cornerFlg = false; //reset the flag
-            
-            lVel = 100;
-            rVel = 100;
-            myMover.cmd_vel(lVel, rVel);
-            
-            delay(550); //FINE TUNE the right turn
-            
-          }
-        
-      } else { //we dont care about the FRONT wall
-          //WALL FOLLOW SUCKA
-          
-          if(shrtL < 5 ) //too close to wall
-          {
-            lVel = 100;
-            rVel = 85; 
-            myMover.follow_vel(lVel, rVel);
-            
-            delay(200);
-            
-            
-          } else if(shrtL > 5){ //too far
-            if(shrtL >= 9)
-            {
-              //LEFT TURN CHECK
-              if(!leftTrnFlg){
-                leftTrnFlg = true;
-                delay(750); //allow the robot to move to the center of the cell to spin to the left
-                continue;  
-              }else{
-                leftTrnFlg = false;
-                
-                
-                myMover.follow_vel(90,90);
-                lcd.setCursor(0,0);
-                lcd.clear();
-                lcd.print("Left Turn Check!");
-                
-                //perform the left turn!
-                lVel = 80;
-                rVel = 80;
-                
-                myMover.follow_vel(lVel, rVel);
-                delay(900);
-
-                //make the robot go straight and then re-evaluate
-                lVel = 95;
-                rVel = 85;
-                myMover.follow_vel(100, 80);
-                delay(750);
-              }
-            }else{ // too FAR
-              lcd.setBacklight(TEAL);
-              lVel = 93;
-              rVel = 85;
-              myMover.follow_vel(lVel, rVel);
-              
-              delay(200);  
+            ++wfCounter;
+            moveAwayFromWall();
+            if(wfCounter == 3)
+            { //offset the extreme moving away
+              moveCloserToWall(); 
+              wfCounter = 0;
             }
             
-            
+          } else if(shrtL >= 6)
+            { //TOO FAR FROM THE WALL
+              if(shrtL >= 9)
+              {
+                //LEFT TURN CHECK
+                if(!leftTrnFlg)
+                {
+                  lcd.setBacklight(RED);
+                  lcd.setCursor(0,0);
+                  lcd.clear();
+                  lcd.print("Left Turn Check!");
+                  leftTrnFlg = true;
+                  //move forward a little
+                  moveForwardALittle();
+                  stopRobot();
+                  continue;  
+                }else
+                {//THIS IS WHERE THE LEFT TURN OCCURS        
+                  leftTrnFlg = false;
+                  
+                  //perform the left turn!
+                  leftTurn();
+                  wfCounter = 0; 
+  
+                }
+            }else
+            { // too FAR
+              lcd.setBacklight(TEAL);
+              ++wfCounter;
+              moveCloserToWall();
+              if(wfCounter == 3)
+              { //offset the moving away
+                //moveAwayFromWall();
+                wfCounter = 0;  
+              }
+            }
           } else if(shrtL == 5){ //aligned with wall
             lcd.setBacklight(WHITE);
-            lVel = 95;
-            rVel = 85;
-            myMover.follow_vel(lVel, rVel);
-            
-            delay(500); //allow robot to update quicker when aligned
+            alignWithWall();
+            //wfCounter = 0;
           }
+         
+      } else//GETTING CLOSE TO WALL FROM FRONT
+      { 
+         //STOP the robot and resample
+        if(shrtL <= 5)//check left sensor to make sure it's a corner
+        {
+          if(!cornerFlg)
+            {
+              lcd.clear();
+              lcd.setBacklight(YELLOW);
+              lcd.print("Corner case check");
+              stopRobot();
+              cornerFlg = true;  
+              continue;
+            }else 
+            { 
+              //CORNER confirmed 
+              cornerFlg = false; //reset the flag
+              rightTurn();
+              wfCounter = 0;
+            }
+        } 
       }
 
     if(leftTrnFlg){
@@ -228,15 +245,68 @@ void Navigation(){
     if(cornerFlg){
       cornerFlg = false;
     }
-    
-    printLCD(shrtF, shrtL, lVel, rVel);
-    //myMover.follow_vel(lVel, rVel);
   }
 }
 
 void rightTurn(){
-  myMover.cmd_vel(100, 100);
-  delay(1000);
   
+  lVel = 100;
+  rVel = 90;
+  myMover.cmd_vel(lVel, rVel);
+  delay(1100);
+  
+}
+
+void leftTurn(){
+  //perform the left turn!
+  //myMover.cmd_vel(90, 80);
+  lVel = 90;
+  rVel = 80;
+  myMover.cmd_vel(lVel, rVel);
+  delay(1200);
+}
+
+
+// <= 5 95, 88
+// >= 7 92, 85
+void moveCloserToWall(){ //too far from wall
+  
+  //myMover.cmd_vel(93, 85);
+  lVel = 95; 
+  rVel = 83; //SPEED right up
+  myMover.cmd_vel(lVel, rVel);
+  //delay(200);  
+}
+
+void moveAwayFromWall(){ //too close to wall
+ // myMover.cmd_vel(95, 87);
+ lVel = 97; //SPEED left up
+ rVel = 86; 
+ myMover.cmd_vel(lVel, rVel);
+ //delay(200);       
+}
+
+void alignWithWall(){ //align
+  //myMover.cmd_vel(95, 85);
+  lVel = 95,
+  rVel = 86; //right motor is weaker
+  myMover.cmd_vel(lVel, rVel);
+  //delay(200); //allow robot to update quicker when aligned
+}
+
+void moveForwardALittle(){//move forward a little before left turn
+  //myMover.cmd_vel(95, 85);//allow robot to move forward a bit before turning left
+  lVel = 95;
+  rVel = 85;
+  myMover.cmd_vel(lVel, rVel);
+  delay(1200); //allow the robot to move to the center of the cell to spin to the left
+}
+
+void stopRobot(){//stops robot
+  //myMover.cmd_vel(90,90);
+  lVel = 90;
+  rVel = 90;
+  myMover.cmd_vel(lVel, rVel);
+  delay(500);
 }
 
